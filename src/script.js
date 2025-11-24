ab055fecc99e769791616747715343194a95b445
Yushi <yushari@seznam.cz>
2025-11-24 15:28:29 +0100

    finished the mobile layout

---
+++ src/script.js
@@ -0,0 +1,61 @@
+// DOM
+const cardContainer = document.querySelector('.card_container');
+const successContainer = document.querySelector('.success_container');
+const successEmail = document.querySelector('.success_email');
+const dismissBtn = document.querySelector('.dismiss_btn');
+const invalidText = document.querySelector('.email_invalid');
+const subscribeBtn = document.getElementById('subscribe_btn');
+const cardForm = document.getElementById('card_form');
+const emailInput = document.getElementById('email');
+// Email Regex to validate email
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+// Functions
+function validateEmail(email) {
+    return emailRegex.test(email);
+}
+
+function showInvalidText() {
+    invalidText.classList.remove('hidden');
+}
+
+function hideInvalidText() {
+    invalidText.classList.add('hidden');
+}
+
+function showMessageSection() {
+   successContainer.classList.remove('hidden');
+}
+
+function hideMessageSection() {
+    successContainer.classList.add('hidden');
+}
+
+function hideCardSection() {
+    cardContainer.classList.add('hidden');
+}
+
+function showCardSection() {
+    cardContainer.classList.remove('hidden');
+}
+
+// Event Listeners
+subscribeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (validateEmail(emailInput.value)) {
+        hideCardSection();
+        showMessageSection();
+        successEmail.textContent = emailInput.value;
+        emailInput.value = '';
+    } else {
+        showInvalidText();
+        emailInput.value = '';
+    }
+})
+
+dismissBtn.addEventListener('click', () => {
+    hideMessageSection();
+    showCardSection();
+
+    invalidText.classList.add('hidden');
+})
